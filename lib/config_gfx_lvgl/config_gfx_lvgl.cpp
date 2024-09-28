00d117f1d3b1cfd64d6b6846114f5e4a26b73955
moisesfa <moifdez@gmail.com>
2024-09-28 11:06:58 +0200

    ordenar codigo para mejorar estructura del proyecto

---
+++ lib/config_gfx_lvgl/config_gfx_lvgl.cpp
@@ -0,0 +1,220 @@
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/9.2/integration/framework/arduino.html  */
+
+#include <lvgl.h>
+
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ *Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ *as the examples and demos are now part of the main LVGL library. */
+
+// #include <examples/lv_examples.h>
+// #include <demos/lv_demos.h>
+
+// #define DIRECT_MODE // Uncomment to enable full frame buffer
+
+#include <Arduino_GFX_Library.h>
+
+#define GFX_DEV_DEVICE WAVESHARE_ESP32_S3_TFT_4_3B
+//define GFX_BL 2
+
+Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+  5 /* DE */,3 /* VSYNC */,46 /* HSYNC */,7 /* PCLK */,
+  1 /* R0 */,2 /* R1 */,42 /* R2 */,41 /* R3 */,40 /* R4 */,
+  39 /* G0 */,0 /* G1 */,45 /* G2 */,48 /* G3 */,47 /* G4 */,21 /* G5 */,
+  14 /* B0 */,38 /* B1 */,18 /* B2 */,17 /* B3 */,10 /* B4 */,
+
+  0 /* hsync_polarity */, 40 /* hsync_front_porch */, 48 /* hsync_pulse_width */, 88 /* hsync_back_porch */,
+  0 /* vsync_polarity */, 13 /* vsync_front_porch */, 3 /* vsync_pulse_width */, 32 /* vsync_back_porch */,
+  1 /* pclk_active_neg */, 16000000 /* prefer_speed */
+);
+
+//void GFXdraw16bitBeRGBBitmap(int16_t x1, int16_t y1, uint16_t* full, int16_t w, int16_t h);
+
+Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+  800 /* width */,
+  480 /* height */,
+  rgbpanel,
+  0 /* rotation */,
+  true /* auto_flush */
+);
+
+/*******************************************************************************
+ * End of Arduino_GFX setting
+ ******************************************************************************/
+
+#include "ui_ex1.h"
+
+/*******************************************************************************
+ * Please config the touch panel in touch.h
+ ******************************************************************************/
+#include "touch.h"
+#include "config_gfx_lvgl.h"
+
+uint32_t screenWidth;
+uint32_t screenHeight;
+uint32_t bufSize;
+lv_display_t *disp;
+lv_color_t *disp_draw_buf;
+
+#if LV_USE_LOG != 0
+void my_print(lv_log_level_t level, const char *buf)
+{
+  LV_UNUSED(level);
+  Serial.println(buf);
+  Serial.flush();
+}
+#endif
+
+uint32_t millis_cb(void)
+{
+  return millis();
+}
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+#ifndef DIRECT_MODE
+  uint32_t w = lv_area_get_width(area);
+  uint32_t h = lv_area_get_height(area);
+
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)px_map, w, h);
+#endif // #ifndef DIRECT_MODE
+
+  /*Call it to tell LVGL you are ready*/
+  lv_disp_flush_ready(disp);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PRESSED;
+
+      /*Set the coordinates*/
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_RELEASED;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+}
+
+
+void config_gfx_lvgl_init()
+{
+  Serial.println("Arduino_GFX LVGL_Arduino_v9 example ");
+  String LVGL_Arduino = String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+  Serial.println(LVGL_Arduino);
+
+#ifdef GFX_EXTRA_PRE_INIT
+  GFX_EXTRA_PRE_INIT();
+#endif
+
+  // Init Display
+  if (!gfx->begin())
+  {
+    Serial.println("gfx->begin() failed!");
+  }
+  gfx->fillScreen(BLACK);
+
+#ifdef GFX_BL
+  pinMode(GFX_BL, OUTPUT);
+  digitalWrite(GFX_BL, HIGH);
+#endif
+
+  // Init touch device
+  touch_init(gfx->width(), gfx->height(), gfx->getRotation());
+
+  lv_init();
+
+  /*Set a tick source so that LVGL will know how much time elapsed. */
+  lv_tick_set_cb(millis_cb);
+
+  /* register print function for debugging */
+#if LV_USE_LOG != 0
+  lv_log_register_print_cb(my_print);
+#endif
+
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+
+#ifdef DIRECT_MODE
+  bufSize = screenWidth * screenHeight;
+#else
+  bufSize = screenWidth * 40;
+#endif
+
+#ifdef ESP32
+#if defined(DIRECT_MODE) && (defined(CANVAS) || defined(RGB_PANEL))
+  disp_draw_buf = (lv_color_t *)gfx->getFramebuffer();
+#else  // !(defined(DIRECT_MODE) && (defined(CANVAS) || defined(RGB_PANEL)))
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+  if (!disp_draw_buf)
+  {
+    // remove MALLOC_CAP_INTERNAL flag try again
+    disp_draw_buf = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_8BIT);
+  }
+#endif // !(defined(DIRECT_MODE) && (defined(CANVAS) || defined(RGB_PANEL)))
+#else // !ESP32
+  Serial.println("LVGL disp_draw_buf heap_caps_malloc failed! malloc again...");
+  disp_draw_buf = (lv_color_t *)malloc(bufSize * 2);
+#endif // !ESP32
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    disp = lv_display_create(screenWidth, screenHeight);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+#ifdef DIRECT_MODE
+    lv_display_set_buffers(disp, disp_draw_buf, NULL, bufSize * 2, LV_DISPLAY_RENDER_MODE_DIRECT);
+#else
+    lv_display_set_buffers(disp, disp_draw_buf, NULL, bufSize * 2, LV_DISPLAY_RENDER_MODE_PARTIAL);
+#endif
+
+    /*Initialize the (dummy) input device driver*/
+    lv_indev_t *indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    lv_indev_set_read_cb(indev, my_touchpad_read);
+
+    /*******************************************************************************
+    * UI
+    ******************************************************************************/
+    ui_example1();
+    
+  
+  }
+
+  Serial.println("Setup done");
+}
+
+void config_gfx_lvgl_loop()
+{
+  lv_task_handler(); /* let the GUI do its work */
+
+#ifdef DIRECT_MODE
+#if defined(CANVAS) || defined(RGB_PANEL)
+  gfx->flush();
+#else // !(defined(CANVAS) || defined(RGB_PANEL))
+  gfx->draw16bitRGBBitmap(0, 0, (uint16_t *)disp_draw_buf, screenWidth, screenHeight);
+#endif // !(defined(CANVAS) || defined(RGB_PANEL))
+#else  // !DIRECT_MODE
+#ifdef CANVAS
+  gfx->flush();
+#endif
+#endif // !DIRECT_MODE
+
+  delay(5);
+
+}
---
+++ lib/ui_ex1/ui_ex1.h
@@ -0,0 +1,3 @@
+#pragma once 
+
+void ui_example1 (void);
