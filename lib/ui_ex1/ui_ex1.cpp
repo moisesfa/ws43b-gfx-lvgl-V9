ed13a9eb6d46f21274921814601a9fbc6a55e1a5
moisesfa <moifdez@gmail.com>
2024-10-04 19:28:55 +0200

    añadir ejemplo de eventos de boton

--- lib/ui_ex1/ui_ex1.cpp
+++ lib/ui_ex1/ui_ex1.cpp
@@ -52,7 +52,13 @@ void ui_example1(void)
     
 }
 
-static void btn3_event_cb(lv_event_t * e) {
+//***********************************
+//EJERCICIO 1
+//***********************************
+/* El siguiente código creará un botón básico y una etiqueta de contador 
+que aumentará cada vez que toque el botón.*/
+
+static void event_handler_e1(lv_event_t * e) {
   lv_event_code_t code = lv_event_get_code(e);
   lv_obj_t * label = (lv_obj_t*) lv_event_get_user_data(e);
   
@@ -65,17 +71,17 @@ static void btn3_event_cb(lv_event_t * e) {
   
 }
 
-void ui_example2(void)
+void lv_create_main_gui_e1(void)
 {
-// Create a Button 
-  lv_obj_t * btn3 = lv_button_create(lv_screen_active());    
-  lv_obj_set_size(btn3, 120, 50);                                  // Set the btn3 size
-  lv_obj_align(btn3, LV_ALIGN_CENTER, 0, 0);
+  // Create a Button 
+  lv_obj_t * btn = lv_button_create(lv_screen_active());    
+  lv_obj_set_size(btn, 220, 80);                                  // Set the btn3 size
+  lv_obj_align(btn, LV_ALIGN_CENTER, 0, 0);
   
   // Add a label to the btn3
-  lv_obj_t * btn3_label = lv_label_create(btn3);     
-  lv_label_set_text(btn3_label, "Click here!");        // Set the labels text
-  lv_obj_center(btn3_label);
+  lv_obj_t * btn_label = lv_label_create(btn);     
+  lv_label_set_text(btn_label, "Click here!");        // Set the labels text
+  lv_obj_center(btn_label);
 
   lv_obj_t * tl_counter = lv_label_create(lv_screen_active());
   lv_label_set_text(tl_counter, "Counter: 0");
@@ -87,5 +93,59 @@ void ui_example2(void)
   lv_style_set_text_font(&style_text_label, &lv_font_montserrat_34); 
   lv_obj_add_style(tl_counter, &style_text_label, 0);
 
-  lv_obj_add_event_cb(btn3, btn3_event_cb, LV_EVENT_ALL, tl_counter);  // Assign a callback to the button
+  lv_obj_add_event_cb(btn, event_handler_e1, LV_EVENT_ALL, tl_counter);  // Assign a callback to the button
 }
+
+//***********************************
+//EJERCICIO 2
+//***********************************
+/* 
+Un botón puede detectar diferentes tipos de eventos, no solo un evento del tipo hecho clic.
+El siguiente código imprime el tipo de evento detectado por el botón en la pantalla.
+*/
+
+static void event_handler_e2(lv_event_t * e) {
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t * label = (lv_obj_t*) lv_event_get_user_data(e);
+
+  switch(code) {
+    case LV_EVENT_PRESSED:
+      lv_label_set_text(label, "The last button event:\nLV_EVENT_PRESSED");
+      break;
+    case LV_EVENT_CLICKED:
+      lv_label_set_text(label, "The last button event:\nLV_EVENT_CLICKED");
+      break;
+    case LV_EVENT_LONG_PRESSED:
+      lv_label_set_text(label, "The last button event:\nLV_EVENT_LONG_PRESSED");
+      break;
+    case LV_EVENT_LONG_PRESSED_REPEAT:
+      lv_label_set_text(label, "The last button event:\nLV_EVENT_LONG_PRESSED_REPEAT");
+      break;
+    default:
+      break;
+  }
+}
+
+void lv_create_main_gui_e2(void) {
+  // Create a Button 
+  lv_obj_t * button = lv_button_create(lv_screen_active());    
+  lv_obj_set_size(button, 220, 80);                                  // Set the button size
+  lv_obj_align(button, LV_ALIGN_CENTER, 0, -30);
+  
+  // Add a label to the button
+  lv_obj_t * button_label = lv_label_create(button);     
+  lv_label_set_text(button_label, "Click here!");        // Set the labels text
+  lv_obj_center(button_label);
+
+  lv_obj_t * text_label_event = lv_label_create(lv_screen_active());
+  lv_label_set_text(text_label_event, "The last button event:\nNONE");
+  lv_obj_align(text_label_event, LV_ALIGN_BOTTOM_MID, 0, -40);
+
+  static lv_style_t style_text_label; 
+  lv_style_init(&style_text_label); 
+  lv_style_set_text_font(&style_text_label, &lv_font_montserrat_34); 
+  lv_obj_add_style(text_label_event, &style_text_label, 0);
+
+  lv_obj_add_event_cb(button, event_handler_e2, LV_EVENT_ALL, text_label_event);  // Assign a callback to the button
+}
+
--- lib/ui_ex1/ui_ex1.h
+++ lib/ui_ex1/ui_ex1.h
@@ -1,4 +1,5 @@
 #pragma once 
 
-void ui_example1 (void);
-void ui_example2 (void);
+void ui_example1(void);
+void lv_create_main_gui_e1(void);
+void lv_create_main_gui_e2(void);
